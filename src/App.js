bc77cbbbda56dfaef031ef473daa721e78f1ea33
CornetS28 <corsam28@gmail.com>
2019-11-25 10:40:36 -0500

    css styling added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import Header from "./component/header";
+import "./app.scss";
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
+  }
 }
 
 export default App;
